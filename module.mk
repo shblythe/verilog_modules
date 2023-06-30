7b99ef623217d727ce97bbfbf98d0fc68b64b328
Stephen Blythe <stephen@urwick.co.uk>
2023-06-30 22:07:19 +0100

    Split make gtkwave and generation of data
    Can now run 'make gtkwave' to launch gtkwave without regenerating the data.
    Data will now only be regenerated if the source files change.

--- module.mk
+++ module.mk
@@ -10,13 +10,16 @@ gui: $(PROJECT_NAME).json
 	nextpnr-ice40 --package cb132 --hx8k --json $(PROJECT_NAME).json \
 		--gui $(PCF_OPTS)
 
-gtkwave: *.v
-	iverilog -I../.. $(IVERILOG_OPTS) -o $(PROJECT_NAME)_tb.out -D VCD_OUTPUT=$(PROJECT_NAME)_tb $(PROJECT_NAME)_tb.v $(PROJECT_NAME).v 
-	vvp $(PROJECT_NAME)_tb.out 
+gtkwave: $(PROJECT_NAME)_tb.vcd
 	gtkwave $(PROJECT_NAME)_tb.vcd $(PROJECT_NAME)_tb.gtkw
 
 $(PROJECT_NAME).json: *.v
 	iverilog -I../.. $(IVERILOG_OPTS) -o hardware.out *.v
+
+$(PROJECT_NAME)_tb.vcd: *.v
+	iverilog -I../.. $(IVERILOG_OPTS) -o $(PROJECT_NAME)_tb.out -D VCD_OUTPUT=$(PROJECT_NAME)_tb $(PROJECT_NAME)_tb.v $(PROJECT_NAME).v
+	vvp $(PROJECT_NAME)_tb.out
+
 	yosys -p 'synth_ice40 -top $(PROJECT_NAME) -json $(PROJECT_NAME).json' \
 		*.v
 
